40a14a15a745ef7c2ed50426f7d8f19bb3bc70a3
AZinkevich <azinkevich@mail.ru>
2024-04-23 19:35:01 +1100

    Header styled

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,51 +1,59 @@
 import { useState } from "react";
+import { Button } from "./Header.styled.js";
 
-export const Header = ({addCard}) => {
-  const [isOpenUser, setIsOpenUser] = useState(false)
+export const Header = ({ addCard }) => {
+  const [isOpenUser, setIsOpenUser] = useState(false);
 
-const toggleOpenUser = (e) => {
-  e.preventDefault()
-  setIsOpenUser(prev => !prev)
-}
+  const toggleOpenUser = (e) => {
+    e.preventDefault();
+    setIsOpenUser((prev) => !prev);
+  };
 
-    return   <header className="header">
-    <div className="container">
-      <div className="header__block">
-        <div className="header__logo _show _light">
-          <a href="" target="_self">
-            <img src="../public/logo.png" alt="logo" />
-          </a>
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header__block">
+          <div className="header__logo _show _light">
+            <a href="" target="_self">
+              <img src="../public/logo.png" alt="logo" />
+            </a>
+          </div>
+          <div className="header__logo _dark">
+            <a href="" target="_self">
+              <img src="../public/logo_dark.png" alt="logo" />
+            </a>
+          </div>
+          <nav className="header__nav">
+            <Button onClick={addCard} id="btnMainNew">
+              Создать новую задачу
+            </Button>
+            <a
+              href="#user-set-target"
+              onClick={toggleOpenUser}
+              className="header__user _hover02"
+            >
+              Ivan Ivanov
+            </a>
+            {isOpenUser && (
+              <div
+                className="header__pop-user-set pop-user-set"
+                id="user-set-target"
+              >
+                {/* <a href="">x</a>  */}
+                <p className="pop-user-set__name">Ivan Ivanov</p>
+                <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
+                <div className="pop-user-set__theme">
+                  <p>Темная тема</p>
+                  <input type="checkbox" className="checkbox" name="checkbox" />
+                </div>
+                <button type="button" className="_hover03">
+                  <a href="#popExit">Выйти</a>
+                </button>
+              </div>
+            )}
+          </nav>
         </div>
-        <div className="header__logo _dark">
-          <a href="" target="_self">
-            <img src="../public/logo_dark.png" alt="logo" />
-          </a>
-        </div>
-        <nav className="header__nav">
-          <button className="header__btn-main-new _hover01" id="btnMainNew">
-            <a href="#popNewCard" onClick={addCard}>Создать новую задачу</a>
-          </button>
-          <a href="#user-set-target" onClick={toggleOpenUser} className="header__user _hover02">
-            Ivan Ivanov
-          </a>
-          {isOpenUser && (<div
-            className="header__pop-user-set pop-user-set"
-            id="user-set-target"
-          >
-             {/* <a href="">x</a>  */}
-            <p className="pop-user-set__name">Ivan Ivanov</p>
-            <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
-            <div className="pop-user-set__theme">
-              <p>Темная тема</p>
-              <input type="checkbox" className="checkbox" name="checkbox" />
-            </div>
-            <button type="button" className="_hover03">
-              <a href="#popExit">Выйти</a>
-            </button>
-          </div>)}
-          
-        </nav>
       </div>
-    </div>
-  </header>
-}
+    </header>
+  );
+};
