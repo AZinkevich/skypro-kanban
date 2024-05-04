dcc74995bf6b02957d749fea281dab5c374b1ed6
AZinkevich <azinkevich@mail.ru>
2024-05-04 15:04:18 +1100

    PopExit styling

--- src/components/Popups/PopExit/PopExit.jsx
+++ src/components/Popups/PopExit/PopExit.jsx
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom";
 import { paths } from "../../../data";
+import * as S from "./PopExit.styled.js"
+
 
 export const PopExit = () => {
   return (
-    <div className="pop-exit" id="popExit">
-      <div className="pop-exit__container">
-        <div className="pop-exit__block">
-          <div className="pop-exit__ttl">
+    <S.PopExit>
+      <S.PopExitContainer>
+        <S.PopExitBlock>
+          <S.PopExitTtl>
             <h2>Выйти из аккаунта?</h2>
-          </div>
+          </S.PopExitTtl>
           <form className="pop-exit__form" id="formExit" action="#">
             <div className="pop-exit__form-group">
               <button className="pop-exit__exit-yes _hover01" id="exitYes">
@@ -19,8 +21,8 @@ export const PopExit = () => {
               </button>
             </div>
           </form>
-        </div>
-      </div>
-    </div>
+        </S.PopExitBlock>
+      </S.PopExitContainer>
+    </S.PopExit>
   );
 }
